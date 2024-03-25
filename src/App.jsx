5a06b569c0fd060b57710ebc699b9c6ff08c989e
Souleymane Sy <souleymanesyservices@gmail.com>
2024-03-26 00:01:51 +0100

    feat: added a range slider and checkbox for the user to toggle price and amount of the pageviews

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,93 @@
+import { Fragment } from "react";
 import { useState } from "react";
 
+const pricingAmounts = {
+  1: {
+    pageViews: "10K",
+    monthly: 8,
+    yearly: 72,
+  },
+  2: {
+    pageViews: "50K",
+    monthly: 12,
+    yearly: 108,
+  },
+  3: {
+    pageViews: "100K",
+    monthly: 16,
+    yearly: 144,
+  },
+  4: {
+    pageViews: "500K",
+    monthly: 24,
+    yearly: 216,
+  },
+  5: {
+    pageViews: "1M",
+    monthly: 36,
+    yearly: 324,
+  },
+};
 function App() {
-  const [slider, setSlider] = useState(0);
-
   return (
     <>
-      <div>Hello From App</div>
+      <InputSlider />
     </>
   );
 }
+
+export const InputSlider = () => {
+  const [pageviews, setPageviews] = useState(0);
+  const [isMonthly, setIsMonthly] = useState(true);
+
+  // const handleInputRange = (e) => {
+  //   setPageviews(e.target.value);
+  // };
+
+  return (
+    <Fragment>
+      <section>
+        <h2>{pricingAmounts[pageviews].pageViews} Pageviews</h2>
+
+        <input
+          type="range"
+          name="range-slider"
+          id="range-slider"
+          value={pageviews}
+          step={1}
+          min={1}
+          max={5}
+          aria-label={pricingAmounts[pageviews].pageViews}
+          onInput={(e) => {
+            setPageviews(e.target.value);
+          }}
+        />
+
+        <p>
+          {pricingAmounts[pageviews][isMonthly ? "monthly" : "yearly"]} /{" "}
+          {isMonthly ? "Month" : "Year"}
+        </p>
+      </section>
+
+      <div>
+        <p>Monthly Billing</p>
+
+        <div className="card--checkbox">
+          <input
+            type="checkbox"
+            id="card--checkbox"
+            onChange={() => {
+              setIsMonthly(!isMonthly);
+            }}
+          />
+        </div>
+
+        <p>
+          Yearly Billing <span>-25%</span>
+        </p>
+      </div>
+    </Fragment>
+  );
+};
 
 export default App;
