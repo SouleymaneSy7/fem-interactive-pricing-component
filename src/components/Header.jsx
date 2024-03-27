eea0d50cd6715ba312db6d2dff913c80a8eb8bd3
Souleymane Sy <souleymanesyservices@gmail.com>
2024-03-27 04:56:24 +0100

    refactor: create InputSlider component and header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header>
+      <h1>Simple, traffic-based pricing</h1>
+      <p>Sign-up for our 30-day trial. No credit card required.</p>
+    </header>
+  );
+};
+  
---
+++ src/components/InputSlider.jsx
@@ -0,0 +1,84 @@
+import { Fragment } from "react";
+import { useState } from "react";
+
+const pricingAmounts = {
+  1: {
+    pageViews: "10K",
+    monthly: 8,
+    yearly: 72,
+  },
+  2: {
+    pageViews: "50K",
+    monthly: 12,
+    yearly: 108,
+  },
+  3: {
+    pageViews: "100K",
+    monthly: 16,
+    yearly: 144,
+  },
+  4: {
+    pageViews: "500K",
+    monthly: 24,
+    yearly: 216,
+  },
+  5: {
+    pageViews: "1M",
+    monthly: 36,
+    yearly: 324,
+  },
+};
+
+export const InputSlider = () => {
+  const [pageviews, setPageviews] = useState(1);
+  const [isMonthly, setIsMonthly] = useState(true);
+
+  const handleInputRange = (e) => {
+    setPageviews(e.target.value);
+  };
+
+  const handleCheckbox = () => {
+    setIsMonthly(!isMonthly);
+  };
+
+  return (
+    <Fragment>
+      <section>
+        <h2>{pricingAmounts[pageviews].pageViews} Pageviews</h2>
+
+        <input
+          type="range"
+          name="range-slider"
+          id="range-slider"
+          value={pageviews}
+          step={1}
+          min={1}
+          max={5}
+          aria-label={`${pricingAmounts[pageviews].pageViews} pageviews`}
+          onInput={handleInputRange}
+        />
+
+        <p>
+          ${pricingAmounts[pageviews][isMonthly ? "monthly" : "yearly"]} /
+          {isMonthly ? "Month" : "Year"}
+        </p>
+      </section>
+
+      <div>
+        <p>Monthly Billing</p>
+
+        <div className="card--checkbox">
+          <input
+            type="checkbox"
+            id="card--checkbox"
+            onChange={handleCheckbox}
+          />
+        </div>
+
+        <p>
+          Yearly Billing <span>-25%</span>
+        </p>
+      </div>
+    </Fragment>
+  );
+};
